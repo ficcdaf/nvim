d113370dda1f393f9e077363ecdd8252bda8541c
Daniel Fichtinger <daniel@ficd.ca>
2024-10-20 22:18:50 -0400

    enable breakindent for markdown

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,7 +2,7 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "norg" },
+  pattern = { "norg", "markdown" },
   callback = function()
     vim.opt_local.breakindent = true
     vim.opt_local.wrap = true
