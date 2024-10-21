19f1e2a94e38daf8adaee69b2e9e4d5365787502
Daniel Fichtinger <daniel@ficd.ca>
2024-10-20 22:20:09 -0400

    Removed "smart action" keybind from obsidian.nvim
    It was just getting annoying and doing stuff I did NOT want it to do;
    however, I do need it to follow links so I will need to revisit this!

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -54,12 +54,12 @@ return {
         opts = { buffer = true },
       },
       -- Smart action depending on context, either follow link or toggle checkbox.
-      ["<cr>"] = {
-        action = function()
-          return require("obsidian").util.smart_action()
-        end,
-        opts = { buffer = true, expr = true },
-      },
+      -- ["<cr>"] = {
+      --   action = function()
+      --     return require("obsidian").util.smart_action()
+      --   end,
+      --   opts = { buffer = true, expr = true },
+      -- },
     },
     new_notes_location = "notes_subdir",
     templates = {
