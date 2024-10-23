5059e5b46257238bbf1a85c032ac4d3805aaa03f
Daniel Fichtinger <daniel@ficd.ca>
2024-10-23 02:20:59 -0400

    disabled scrolloff, added cursor hop keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,11 +3,40 @@
 -- Add any additional keymaps here
 local map = vim.keymap.set
 
+local dirmap = {
+  up = "w0",
+  down = "w$",
+}
+
+local function move_cursor_to(direction)
+  vim.api.nvim_win_set_cursor(0, { vim.fn.line(dirmap[direction]), 0 })
+end
+
+-- TODO: finish this
+-- local function scroll(direction)
+--   -- current window for now; could extend later
+--   local win_id = 0
+--   -- in form {line, column}
+--   local current_cursor = vim.api.nvim_win_get_cursor(win_id)
+--
+--   -- this is the height of the window in lines
+--   local win_height = vim.api.nvim_win_get_height(win_id)
+--
+--   local new_top = vim.fn.line(dirmap[direction])
+--
+-- end
+
 -- Bind arrows to hjkl to my colemak-dh motions work as expected
 map({ "n", "x" }, "<Up>", "k", { desc = "Up", remap = true })
 map({ "n", "x" }, "<Down>", "j", { desc = "Down", remap = true })
 map({ "n", "x" }, "<Left>", "h", { desc = "Left", remap = true })
 map({ "n", "x" }, "<Right>", "l", { desc = "Right", remap = true })
+map({ "n", "x" }, "<PageDown>", function()
+  move_cursor_to("down")
+end, { desc = "Move cursor to bottom", remap = true })
+map({ "n", "x" }, "<Pageup>", function()
+  move_cursor_to("up")
+end, { desc = "Move cursor to top", remap = true })
 map({ "n", "t" }, "<C-Left>", "<C-H>", { desc = "Switch Window Left", remap = true })
 map({ "n", "t" }, "<C-Right>", "<C-L>", { desc = "Switch Window Right", remap = true })
 map({ "n", "t" }, "<C-Up>", "<C-K>", { remap = true })
@@ -21,6 +50,7 @@ map({ "n" }, "<S-Right>", "L", { desc = "Right Buffer", remap = true })
 map({ "n" }, "<S-Down>", "5j", { remap = true })
 map({ "n" }, "<S-Up>", "5k", { remap = true })
 map({ "i" }, "<M-e>", "<Esc>", { desc = "Escape insert mode", remap = true })
+-- <PageDown><PageUp>
 
 -- Remap spelling suggestions
 -- map({ "n" }, "z-", "z=", { desc = "Spelling Suggestions", remap = true })
